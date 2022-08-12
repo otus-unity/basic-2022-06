d03240b4a6e9a0256c78b751a020dd73dd6713a7
justalexi <contact@justalexi.com>
2022-08-13 00:04:28 +0300

    naive level loop (broken)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,25 @@
 using UnityEngine;
 
+// Show menu
+// Player selects "Start Game"
+// Show game intro
+/* Game loop start */
+// Load level 1
+// Show level 1 intro
+
+/* Level loop start */
+// Character 1 Turn
+// Character 2 Turn
+// ...
+// Character N Turn
+/* Level loop finish */
+
+// Load level 2
+// ...
+
+/* Game loop finish */
+
+// Show game over (success or fail)
 public class GameController : MonoBehaviour
 {
     public GameObject player;
@@ -7,25 +27,10 @@ public class GameController : MonoBehaviour
     
     private void Start()
     {
-        // Show menu
-        // Player selects "Start Game"
-        // Show game intro
-        /* Game loop start */
-        // Load level 1
-        // Show level 1 intro
-        
-        /* Level loop start */
-        // Character 1 Turn
-        // Character 2 Turn
-        // ...
-        // Character N Turn
-        /* Level loop finish */
-        
-        // Load level 2
-        // ...
-        
-        /* Game loop finish */
-        
-        // Show game over (success or fail)
+        // while (player.Health > 0 && enemy.Health > 0)
+        // {
+        //     player.Attack(enemy);
+        //     enemy.Attack(player);
+        // }
     }
 }
