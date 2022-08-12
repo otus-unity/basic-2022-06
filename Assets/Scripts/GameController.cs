01e3f3e48cd3b96de8176b9a45bfe335a080ef5a
justalexi <contact@justalexi.com>
2022-08-12 23:59:35 +0300

    add game controller and game loop outline

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private void Start()
+    {
+        // Show menu
+        // Player selects "Start Game"
+        // Show game intro
+        /* Game loop start */
+        // Load level 1
+        // Show level 1 intro
+        
+        /* Level loop start */
+        // Character 1 Turn
+        // Character 2 Turn
+        // ...
+        // Character N Turn
+        /* Level loop finish */
+        
+        // Load level 2
+        // ...
+        
+        /* Game loop finish */
+        
+        // Show game over (success or fail)
+    }
+}
